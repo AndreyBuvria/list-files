74440b4cd8c91a3cdf486f0c8297ec3a67664323
Andrew Buvria <buvrya228@gmail.com>
2022-10-04 18:45:18 +0300

    feat: provide list method

--- listpy/listmodel.py
+++ listpy/listmodel.py
@@ -9,6 +9,27 @@ class ListModel:
         self.search_argument = 'f'
         self.root_path = os.getcwd()
 
+    def get_path(self, file_name):
+        """
+        Return full path of file
+        """
+        return os.path.join(self.root_path, file_name)
+
+    def write_list(self):
+        """
+        Retrieve file names based on provided searching argument
+        and write them down in a text doc
+        """
+        if self.search_argument == 'f':
+            file_names = (entrie for entrie in os.listdir(self.root_path) if os.path.isfile(self.get_path(entrie)))
+        elif self.search_argument == 'd':
+            file_names = (entrie for entrie in os.listdir(self.root_path) if os.path.isdir(self.get_path(entrie)))
+        else:
+            file_names = (entrie for entrie in os.listdir(self.root_path))
+        
+        with open(f'{self.output_file_name}.txt', 'w') as f:
+            for it in file_names:
+                f.write(it + '\n')
 
             
 
--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-from typing import List
 from listpy.listmodel import ListModel
 
+def main():
+    obj = ListModel()
+
+if __name__ == '__main__':
+    main()
