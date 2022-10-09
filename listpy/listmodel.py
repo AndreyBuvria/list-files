92797534987d9165e08e773d49584cb5a231a15c
Andrew Buvria <buvrya228@gmail.com>
2022-10-09 15:27:40 +0300

    feat: implement write_list method

--- listpy/listmodel.py
+++ listpy/listmodel.py
@@ -1,19 +1,17 @@
 import os
 
 class ListModel:
-    def __init__(self, scan_path=None, output_file_name='listModels', input_file_extension=None):
-        self.scan_path = scan_path
-        self.output_file_name = output_file_name
+    def __init__(self, scan_path=None, search_argument=None, output_file_name=None, input_file_extension=None):
+        self.scan_path = os.getcwd() if scan_path is None else scan_path
+        self.output_file_name = 'listModels' if output_file_name is None else output_file_name
+        self.search_argument = search_argument
         self.input_file_extension = input_file_extension
-
-        self.search_argument = 'f'
-        self.root_path = os.getcwd()
 
     def get_path(self, file_name):
         """
         Return full path of file
         """
-        return os.path.join(self.root_path, file_name)
+        return os.path.join(self.scan_path, file_name)
 
     def write_list(self):
         """
@@ -21,17 +19,14 @@ class ListModel:
         and write them down in a text doc
         """
         if self.search_argument == 'f':
-            file_names = (entrie for entrie in os.listdir(self.root_path) if os.path.isfile(self.get_path(entrie)))
+            file_names = (entrie for entrie in os.listdir(self.scan_path) if os.path.isfile(self.get_path(entrie)))
         elif self.search_argument == 'd':
-            file_names = (entrie for entrie in os.listdir(self.root_path) if os.path.isdir(self.get_path(entrie)))
+            file_names = (entrie for entrie in os.listdir(self.scan_path) if os.path.isdir(self.get_path(entrie)))
         else:
-            file_names = (entrie for entrie in os.listdir(self.root_path))
-        
+            file_names = (entrie for entrie in os.listdir(self.scan_path))
+
         with open(f'{self.output_file_name}.txt', 'w') as f:
             for it in file_names:
                 f.write(it + '\n')
 
             
-
-        
-
