f25bda2bd4930bb57d9439da58d4003ea9bf778c
Andrew Buvria <buvrya228@gmail.com>
2022-10-09 15:26:04 +0300

    feat: get args and pass command params

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
+import getopt, sys
 from listpy.listmodel import ListModel
 
 def main():
-    obj = ListModel()
+    argv = sys.argv[1:]
+
+    scan_path = None
+    output_file_name = None
+    input_file_extension = None
+    search_argument = None        
+
+    try:
+        opts, args = getopt.getopt(argv, 'p:o:i:s:')
+    except:
+        print('error')
+
+    for opt, arg in opts:
+        if opt in ['-p']:
+            scan_path = arg
+        elif opt in ['-o']:
+            output_file_name = arg
+        elif opt in ['-i']:
+            input_file_extension = arg
+        elif opt in ['-s']:
+            search_argument = arg
+    o = ListModel(scan_path, search_argument, output_file_name, input_file_extension)
+    o.write_list()
 
 if __name__ == '__main__':
     main()
